8cfe70cc9ace6eff9705f2c57b8efd8a30d7d3b5
PanchenkoOlexandr <Stalker_2604@ukr.net>
2020-04-04 17:02:14 +0300

    lab3/17

--- 3,17.cpp
+++ 3,17.cpp
@@ -2,22 +2,22 @@
 #include <math.h>
 using namespace std;
 int main(){
-	cout << "Calculate approximately the value of an infinite sum with an accuracy of 0.0005:" << endl;
-	cout << "S = 1+...+(-1)^k*(n/((k+1)*(k+2)*(k+3)))^2k +..." << endl << "Input n = ";
+	double exp = 0.0005;
+	double S = 1, Ans = 1;
+	int k = 1;
+	cout << "Calculate approximately the value of an infinite sum with an accuracy of 0.0005:" 
+		<< "S = 1+...+(-1)^k*(n/((k+1)*(k+2)*(k+3)))^2k +..." 
+		<< endl << "Input n = ";
 	int n;
 	cin >> n;
 	cout << "Value exp = 0.0005" << endl;
-	const double exp = 0.0005;
-	double S = 1;
-	double Ans = 1;
-	int k = 1;
-	while (abs(Ans>exp)){
+		while (abs(Ans>exp)){
 		double K = (k + 1) * (k + 2) * (k + 3);
 		Ans = pow(n / K, k * 2);
 		if (k % 2 != 0){Ans = -1 * Ans;}
 		S += Ans;
 		k++;
 	}
-	cout << "The result S: " << fixed << S << endl;
+	cout << "Answer S: " << S << endl;
 	return 0;
 }
